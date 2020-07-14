fa3b80691b82c5ae6dbdbf7c90d091d835a0b959
Jimmie Johansson <jimmitjoo@gmail.com>
2020-07-14 11:55:41 +0200

    Fix some typing issues

--- sam-app/libs/Workout/workout.go
+++ sam-app/libs/Workout/workout.go
@@ -2,19 +2,80 @@ package Workout
 
 import (
 	"../Database"
+	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
 )
 
-
 type WorkoutBody struct {
-	StartingAt string `json:"starting_at"`
-	Activity string `json:"activity"`
-	Name string `json:"name"`
-	Description string `json:"description"`
-	Latitude float64 `json:"latitude"`
-	Longitude float64 `json:"longitude"`
-	UserId int `json:"user_id"`
+	StartingAt  string  `json:"starting_at"`
+	Activity    string  `json:"activity"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Latitude    float64 `json:"latitude"`
+	Longitude   float64 `json:"longitude"`
+	UserId      int     `json:"user_id"`
+}
+
+func Fetch(latitude float64, longitude float64, todayDate string, untilDate string) ([]WorkoutBody, error) {
+	db := Database.Connect()
+
+	var query = "SELECT id, starting_at, activity, `name`, description, latitude, longitude FROM workouts " +
+		"WHERE starting_at >= '" + todayDate + "' AND starting_at < '" + untilDate + "' LIMIT 25"
+
+	rows, err := db.Query(query)
+	if err != nil {
+		log.Println(query)
+		panic(err)
+	}
+
+	var workouts []WorkoutBody
+
+	defer rows.Close()
+	for rows.Next() {
+		var id int
+		var starting_at string
+		var activity string
+		var name sql.NullString
+		var description sql.NullString
+		var lat sql.NullFloat64
+		var long sql.NullFloat64
+
+		err = rows.Scan(&id, &starting_at, &activity, &name, &description, &lat, &long)
+		if err != nil {
+			log.Println("Scanning rows went wrong")
+			panic(err)
+		}
+
+		var workoutName = ""
+		if name.Valid {
+			workoutName = name.String
+		}
+		var workoutDescription = ""
+		if description.Valid {
+			workoutDescription = description.String
+		}
+		var workoutLatitude float64
+		if lat.Valid {
+			workoutLatitude = lat.Float64
+		}
+		var workoutLongitude float64
+		if long.Valid {
+			workoutLongitude = long.Float64
+		}
+
+		var workout = WorkoutBody{
+			StartingAt:  starting_at,
+			Activity:    activity,
+			Name:        workoutName,
+			Description: workoutDescription,
+			Latitude:    workoutLatitude,
+			Longitude:   workoutLongitude,
+		}
+		workouts = append(workouts, workout)
+	}
+
+	return workouts, nil
 }
 
 func Create(data WorkoutBody) (int, error) {
--- sam-app/list-workouts/main.go
+++ sam-app/list-workouts/main.go
@@ -1,1 +1,70 @@
-package list_workouts
+package main
+
+import (
+	"../libs/Workout"
+	"encoding/json"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"log"
+	"strconv"
+)
+
+type body struct {
+	Workouts []Workout.WorkoutBody `json:"workouts"`
+	Message  string                `json:"message"`
+}
+
+type postBody struct {
+	Latitude  string `json:"latitude"`
+	Longitude string `json:"longitude"`
+	TodayDate string `json:"today_date"`
+	UntilDate string  `json:"until_date"`
+}
+
+func main() {
+	lambda.Start(handler)
+}
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	var bodyMessage []byte
+	var statusCode int
+	var err error = nil
+
+	var postData postBody
+	log.Print(request.Body)
+	err = json.Unmarshal([]byte(request.Body), &postData)
+	if err != nil {
+		bodyMessage, _ = json.Marshal(body{
+			Message: "Invalid postdata.",
+		})
+		statusCode = 400
+	} else {
+		lat, err := strconv.ParseFloat(postData.Latitude, 64)
+		if err != nil {
+			panic(err)
+		}
+		long, err := strconv.ParseFloat(postData.Longitude, 64)
+		if err != nil {
+			panic(err)
+		}
+
+		workouts, err := Workout.Fetch(lat, long, postData.TodayDate, postData.UntilDate)
+		if err != nil {
+			bodyMessage, _ = json.Marshal(body{
+				Message: "Could not fetch workouts.",
+			})
+			statusCode = 400
+		} else {
+			bodyMessage, _ = json.Marshal(body{
+				Workouts: workouts,
+			})
+			statusCode = 200
+		}
+	}
+
+	return events.APIGatewayProxyResponse{
+		Body:       string(bodyMessage),
+		StatusCode: statusCode,
+	}, err
+}
